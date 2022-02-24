8e967cb90743d78afa420037ffe328393ee30ca8
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-02-24 16:59:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+let express = require("express");
+let https = require('https');
 
+app = express();
+
+app.get("/", (req, res) => {
+  https.get(req.query["url"], (resp) => {
+  let data = '';
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+  resp.on('end', () => {
+    res.set('Content-Type', 'text/text');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(data);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+});
+app.listen(5000, (req, res) => {
+  console.log("Running on port 5000");
+});
